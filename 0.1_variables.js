7ece186e234f501abc6afc0c3004ee440d70fa10
Ashutosh Yadav <ashutosh_yadav@Ashutoshs-MacBook-Air.local>
2023-08-19 12:59:19 +0530

    learning about const , var , and let in an experimental way

---
+++ 0.1_variables.js
@@ -0,0 +1,16 @@
+const accountID = 12232;
+let accountEmail = "ashutosh@gmail.com";
+var accountPassword = "*******";
+
+accountCity = "Mathura"
+accountPassword = "hey there!"
+let accountState;
+
+// accountID  = "hello";
+accountEmail = 234323;
+
+accountCity = "jaipur";
+
+console.table([accountCity , accountPassword , accountEmail , accountState])
+
+// prefer not to use var because of issue in functional scope and block scope

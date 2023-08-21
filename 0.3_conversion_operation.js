78145c238059a70abb603e9b364db63ed66f62ad
Ashutosh Yadav <ashutosh_yadav@Ashutoshs-MacBook-Air.local>
2023-08-21 14:09:11 +0530

    learned about operations and how operations can be confusing

--- 0.3_conversion_operation.js
+++ 0.3_conversion_operation.js
@@ -11,15 +11,52 @@ console.log(valueInNumber)
 // true => 1 , false => 0
 
 
-let isLoggedIn = "Hello"
-let BooleanLoggedIn = Boolean(isLoggedIn)
-console.log(BooleanLoggedIn);
+// let isLoggedIn = "Hello"
+// let BooleanLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanLoggedIn);
 
 
 // if you are writing 1 => true , "" => false , "abc" => true 
 // all these are boolean variations.
 
-let someNumber = 33;
-let checkString = String(someNumber)
-console.log(checkString);
-console.log(typeof checkString);
+// let someNumber = 33;
+// let checkString = String(someNumber)
+// console.log(checkString);
+// console.log(typeof checkString);
+
+
+// ********************** Operations ******************************
+
+// Important operations
+// let value = 3;
+// let NegValue = -value;
+// console.log(NegValue);
+
+// Basics
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+
+// **** confusing oerations ******
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 3) // if string come first then it will output => 123
+console.log(1 + 2 + "3") // if Integer comes first then it will add first then string is being added after it output => 33
+
+
+// Bad way pf writing code
+
+console.log(+true) 
+console.log(-true)
+console.log(+"")
+// all these are not industry standards 
+
+// study about prefix and postfix MDN docs
+// link - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+

96f1f7f04b0d63a5530f738fefa560c7de9d3fa7
Ashutosh Yadav <ashutosh_yadav@Ashutoshs-MacBook-Air.local>
2023-09-18 17:23:04 +0530

    use of promise , try , catch , finally => how to use them , little bit about frtch and async await

---
+++ Advance/promises.js
@@ -0,0 +1,138 @@
+const promiseOne = new Promise((resolve , reject) => {
+
+    // DB calls
+    // network handelling
+    // any cryptography work
+
+    setTimeout(() => {
+        console.log('Async task is completed')
+        resolve()
+        
+    },1000)
+
+})
+
+const promiseConsumed = promiseOne.then(()=> {
+    console.log('promise is consumed')
+    
+}) 
+
+
+// other way of doing this
+
+new Promise((resolve , reject)=>{
+    
+    setTimeout(()=>{
+        console.log('async2 is done')
+        resolve()
+    },1000)
+    
+}).then(()=>{
+    console.log('Async 2 resolved')
+
+})
+
+// 3rd promise getting data from object
+
+const promiseThree = new Promise((resolve , reject) => {
+    setTimeout(() => {
+        resolve({username : "Ashutoshyadav" , email : "ashutoshyadav6969@gmail.com"}) // mainly there will be data to be given where the promise will consume  
+    },1000)
+})
+
+promiseThree.then((user) => {
+console.log(user)
+
+})
+
+// other way
+
+const  promiseFour = new Promise((resolve , reject) => {
+    setTimeout(() => {
+        let error = true
+        if (!error) {
+            resolve({username:"ashutosh",age:24,email:"ashutoshyadav6969@gmail.com"}) // getting data from DB
+            
+        }else{
+            reject('ERROR: something went wrong')
+        }
+    } , 2000)
+})
+
+// promiseFour
+.then((user)=>{
+    console.log(user)
+    return user.username
+})
+.then((username)=>{
+    console.log(username)
+
+})
+.catch((error)=>{
+    console.log(error)
+})
+.finally(()=> console.log('Finally  either the promise has been resolved or rejected!'))
+
+// other way => async await same as above but using async await rather using then or catch
+
+const promiseFive = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        let error = true
+        if (!error) {
+            resolve({course:"Javascript", password:123}) // getting data from DB
+            
+        }else{
+            reject('ERROR: JS went wrong')
+        }
+    } , 2000)
+})
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive
+        console.log(response.course);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+
+// getting bunch of users from a JSON file using async await
+
+// async function getAllusers() {
+//     try {
+//         const response = await fetch('https://randomuser.me/api/')
+//         // console.log(response);
+
+//         const data = await response.json()
+//         console.log(data);
+
+//     } catch (error) {
+//         console.log('E: ' , error);
+//     }
+
+// }
+// getAllusers()
+
+// if you want to the same above in then , catch way
+
+
+ 
+
+
+
+fetch('https://randomuser.me/api/')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+
+
